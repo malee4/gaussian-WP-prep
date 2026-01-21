9f0b2217c60fba08ea6f15e4b933e10f49d03158
Melody Lee <melody.lee357@gmail.com>
2026-01-21 10:35:41 -0500

    add draw mcmff2 method

--- tools.py
+++ tools.py
@@ -8,6 +8,16 @@ from pytket.circuit.logic_exp import (
 )
 
 def get_2d_gaussian_coeff(lx, ly, k0, x0, sigma, epsilon=0.0):
+	"""
+	Docstring for get_2d_gaussian_coeff
+	
+	:param lx: Description
+	:param ly: Description
+	:param k0: Description
+	:param x0: Description
+	:param sigma: Description
+	:param epsilon: Description
+	"""
 	# Define momentum eigenstates
 	if lx % 2 == 0:
 		kxs = np.arange(- int(lx / 2) + 1, int(lx / 2) + 1) * 2 * np.pi / lx
@@ -56,13 +66,13 @@ def get_2d_gaussian_coeff(lx, ly, k0, x0, sigma, epsilon=0.0):
 	return c
 	
 
-def get_unitary_thetas(d, c=None, tol=None):
+def get_unitary_thetas(N, c=None, tol=None):
 	""" 
 	Calculates the CRy rotations for a log-scaling unitary circuit for state preparation on 
 	a device with all-to-all connectivity.
 
 	args:
-		d (int):		wavepacket size
+		N (int):		wavepacket size
 		c (np.ndarray):	expected coefficients. If None is passed, the uniform probability distribution is assumed.
 	
 	returns:
@@ -70,24 +80,24 @@ def get_unitary_thetas(d, c=None, tol=None):
 	"""
 	# sympy
 	if c is None:
-		c = np.full(d, 1 / np.sqrt(d))
+		c = np.full(N, 1 / np.sqrt(N))
 	
 	if tol is None:
 		tol = 1e-6
 
-	ry_theta = [Symbol(f't{i}') for i in range(0, d-1)]
+	ry_theta = [Symbol(f't{i}') for i in range(0, N-1)]
 
 	# construct \Theta
-	big_theta = np.ones((d, d), dtype=object)
+	big_theta = np.ones((N, N), dtype=object)
 
-	for n in range(d):
-		for j in range(0, min(2**n, d - 2**n)):
+	for n in range(N):
+		for j in range(0, min(2**n, N - 2**n)):
 			big_theta[j][2**n + j] = cos(ry_theta[2**n + j - 1] / 2)
 			big_theta[2**n + j][2**n + j] = sin(ry_theta[2**n + j - 1] / 2)
 
-	for n_p in range(1, d):
-		for n in range(n_p, d):
-			for j in range(0, min(2**n, d - 2**n) - 1 + 1):
+	for n_p in range(1, N):
+		for n in range(n_p, N):
+			for j in range(0, min(2**n, N - 2**n) - 1 + 1):
 				for jp in range(j, -1, -2):
 					big_theta[2**n + j][jp] = big_theta[2**(n - n_p) + j - 1][2**(n - n_p) + jp - 1]
 					big_theta[2**n + j][2**(n - n_p) + jp] = big_theta[j][2**(n - n_p) + jp]
@@ -106,7 +116,7 @@ def get_unitary_thetas(d, c=None, tol=None):
 		# add to list of equations
 		equation_sys.append(equation)
 
-	guess = np.ones(d-1) # something arbitrary
+	guess = np.ones(N-1) # something arbitrary
 	sol = nsolve(equation_sys, ry_theta, guess, tol=tol)
 	sol = np.array(sol, dtype=float).T[0] # reshape
 
